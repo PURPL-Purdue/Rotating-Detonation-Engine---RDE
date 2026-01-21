d8c767d4957c3246c0b80faee565a791419520a5
niademz <144573692+niademz@users.noreply.github.com>
2026-01-21 16:10:32 -0500

    MATLAB Wrapper CEA for Det

--- HADES/testing.m
+++ HADES/testing.m
@@ -1,7 +1,12 @@
-%example of the ceaRocket function being used
-r = ceaRocket('ox','Air','fuel','H2','phi',1,'Pc',1000,'PcUnits','psia');
-disp(r.isp); % m/s
-disp(r.ivac); % m/s
-disp(r.cstar); % m/s
-disp(r.cf);
-disp(r.AeAt);
+clc; clear;
+
+d = ceaDet('ox','O2','fuel','H2','phi',1,'P0',1000,'P0Units','psia','T0',300,'T0Units','K' );
+
+fprintf('\nCEA DETONATION RESULTS\n');
+
+fprintf('Detonation Mach Number = %.4f\n', d.detMach);
+fprintf('Detonation Velocity = %.2f m/s\n', d.detVel);
+fprintf('P/P1 = %.4f\n', d.P_ratio);
+fprintf('T/T1 = %.4f\n', d.T_ratio);
+fprintf('Burned Pressure = %.2f bar\n', d.P_burned_bar);
+fprintf('Burned Temp = %.2f K\n', d.T_burned_K);
