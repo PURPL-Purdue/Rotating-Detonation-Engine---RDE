a912ef9a699f64808ed379989108633ce9d25836
ahpatel876 <ahpatel876@gmail.com>
2026-01-20 22:00:55 -0500

    cell size and annulus stuff

---
+++ HADES/SIZING MODEL/HADES_size_main_v1.m
@@ -0,0 +1,12 @@
+% cell size
+lambda = 0.2645; %in
+
+%det height
+h = 2.03; %in - eventually put the fuckass shit in
+
+% wtv K is
+K = 5;
+
+%min dia (in)
+d_min = h * K / pi; % in
+
---
+++ HADES/SIZING MODEL/cell_size.m
@@ -0,0 +1,41 @@
+clear; clc; close;
+% Author: anjali
+% vals from caltech thing
+% (https://shepherd.caltech.edu/detn_db/html/H2-Air2.html)
+
+%inputs RAHRAIOEFLKAS
+data = readmatrix("cell_size_daytaa.xlsx");
+chosen_phi = 0.9;
+
+phis = data(:, 1);
+cell_width = data(:, 2);
+
+log_phis = log10(phis);
+log_cell_width = log10(cell_width);
+
+coeffs = polyfit(log_phis, log_cell_width, 3);
+log_phis_fit = linspace(min(log_phis), max(log_phis), 100);
+log_cw_fit = polyval(coeffs, log_phis_fit);
+
+figure();
+scatter(log_phis, log_cell_width);
+hold on;
+plot(log_phis_fit, log_cw_fit);
+xlabel("log(equivalence ratio)");
+ylabel("log(detonation cell width)");
+title("detonation cell width vs equivalence ratio");
+grid on;
+
+
+log_chosen_phi = log10(chosen_phi);
+log_chosen_cellwidth = polyval(coeffs, log_chosen_phi);
+chosen_cellwidth = 10 ^ log_chosen_cellwidth;
+
+% annulus gap correlation from karasu continuous spin detonation
+annulus_gap = 1.4 * chosen_cellwidth; 
+
+fprintf("Chosen equivalence ratio: %.1f. \n", chosen_phi);
+fprintf("Calculated Cell Width: %.3f mm \n", chosen_cellwidth);
+fprintf("Annulus Gap: %.3f mm \n", annulus_gap);
+
+
