ee5590c80c0756001bd10602bd22d9ed1ec8b5eb
niademz <144573692+niademz@users.noreply.github.com>
2026-02-03 15:32:00 -0500

    idk

--- HADES/SIZING MODEL/testing.m
+++ HADES/SIZING MODEL/testing.m
@@ -4,3 +4,9 @@
 
 d = HADES_size_ceaDet('ox','Air','fuel','H2','phi',0.9,'P0',26,'P0Units','psia','T0',283,'T0Units','K');
 
+r_i_in = 2; %internal radius
+r_o_in = 2.25; %outer radius 
+p_o_MPa = 0.101325; %ambient pressure
+P_cj_bar = 30.7; %CJ pressure
+
+res = HADES_size_HoopStressTemps(r_i_in, r_o_in, p_o_MPa, P_cj_bar);
